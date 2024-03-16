43189941a7a5b298439c300af655d400e1c9af8d
tetsunari <mtst.tty556@gmail.com>
2024-03-16 23:37:15 +0900

    userを並べる

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <h1>Users</h1>
+    <>
+      <h1>Users</h1>
+      <ul>
+        <li>User</li>
+        <li>User</li>
+        <li>User</li>
+      </ul>
+    </>
   )
 }
