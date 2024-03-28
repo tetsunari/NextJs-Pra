9db8d1e614bf61e54b5d13422741013d5d21dec0
tetsunari <mtst.tty556@gmail.com>
2024-03-28 23:39:17 +0900

    Linkコンポーネント

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1 className="text-lg border-b pb-1 mb-1">Users</h1>
       <ul className="list-disc ml-4">
-        <li>User</li>
-        <li>User</li>
-        <li>User</li>
+        <li>
+          <Link href="/users/0" className="text-blue-500 hover:text-blue-700">
+            User 0
+          </Link>
+        </li>
+        <li>
+          <Link href="/users/1" className="text-blue-500 hover:text-blue-700">
+            User 1
+          </Link>
+        </li>
+        <li>
+          <Link href="/users/2" className="text-blue-500 hover:text-blue-700">
+            User 2
+          </Link>
+        </li>
       </ul>
     </>
   )
--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 type Props = {
   params: { userId: string };
 }
@@ -8,6 +10,11 @@ export default function UserPage(props: Props) {
       <h1 className="text-lg border-b pb-1 mb-1">
         User Page {props.params.userId}
       </h1>
+      <p>
+        <Link href="/" className="text-blue-500 hover:text-blue-700">
+          Go back
+        </Link>
+      </p>
     </>
   )
 }
