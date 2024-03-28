34fd4084056b4e314305b15f6813873ad2e812a3
tetsunari <mtst.tty556@gmail.com>
2024-03-28 23:56:24 +0900

    usersから一覧表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,21 @@
 import Link from "next/link";
+import { users } from "./lib/users";
 
 export default function Home() {
+  const userItems = users.map((user) => {
+    return (
+      <li key={user.id}>
+        <Link href={`/users/${user.id}`} className="text-blue-500 hover:text-blue-700">
+          {user.name}
+        </Link>
+      </li>
+    );
+  });
   return (
     <>
       <h1 className="text-lg border-b pb-1 mb-1">Users</h1>
       <ul className="list-disc ml-4">
-        <li>
-          <Link href="/users/0" className="text-blue-500 hover:text-blue-700">
-            User 0
-          </Link>
-        </li>
-        <li>
-          <Link href="/users/1" className="text-blue-500 hover:text-blue-700">
-            User 1
-          </Link>
-        </li>
-        <li>
-          <Link href="/users/2" className="text-blue-500 hover:text-blue-700">
-            User 2
-          </Link>
-        </li>
+        {userItems}
       </ul>
     </>
   )
