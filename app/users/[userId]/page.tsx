478d047932d62504f91a46e1b291b73016424711
tetsunari <mtst.tty556@gmail.com>
2024-03-28 23:32:05 +0900

    ダイナミックルーティング

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,7 +1,13 @@
-export default function UserPage() {
+type Props = {
+  params: { userId: string };
+}
+
+export default function UserPage(props: Props) {
   return (
     <>
-      <h1 className="text-lg border-b pb-1 mb-1">User Page</h1>
+      <h1 className="text-lg border-b pb-1 mb-1">
+        User Page {props.params.userId}
+      </h1>
     </>
   )
 }
